f7f5f9e85dbdd22552c5683fa976d5df1517cd1d
Greg Maggard <gregamaggard@gmail.com>
2022-04-11 15:50:03 -0500

    Updating function and control structures exercises.

--- function_exercises.py
+++ function_exercises.py
@@ -5,3 +5,12 @@ def is_two(number):
         return(False)
 
 print(is_two(3))
+
+# Define a function named is_vowel. It should return True if the passed string is a vowel, False otherwise.
+def is_vowel(letter):
+    if letter in ('a', 'e', 'i', 'o', 'u'):
+        return True
+    else:
+        return False
+
+print(is_vowel('p'))
